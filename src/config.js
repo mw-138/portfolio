5e03c2b701883a4fa3bc0aef683f665a582e9b06
mw138 <mattheww138@gmail.com>
2021-08-25 22:05:54 +0100

    feat(config): Added config file

---
+++ src/config.js
@@ -0,0 +1,105 @@
+import MoneyCatcherThumbnail from './assets/images/project_thumbnails/money_catcher.png';
+import RPGClickerThumbnail from './assets/images/project_thumbnails/rpg_clicker.png';
+import WaveSurvivalThumbnail from './assets/images/project_thumbnails/wave_survival.jpeg';
+import EscapeRoomThumbnail from './assets/images/project_thumbnails/escape_room_game.jpeg';
+import AndroidPlatformerThumbnail from './assets/images/project_thumbnails/college_android_game.jpg';
+import MonogameProjectThumbnail from './assets/images/project_thumbnails/monogame_project.png';
+import VRShooterThumbnail from './assets/images/project_thumbnails/vr_shooter_game.jpeg';
+import ProceduralToolThumbnail from './assets/images/project_thumbnails/procedural_tool.png';
+import CLSThumbnail from './assets/images/project_thumbnails/cls.jpg';
+
+export const projectList = [
+  {
+    title: "Money Catcher",
+    platform: "PC",
+    engine: "Unity",
+    language: "C#",
+    description: "Money Catcher is a game where you catch money falling from the sky and obtain a score as high as possible in 60 seconds.",
+    link: "https://lionstargames.itch.io/money-catcher",
+    screenshots: [
+      MoneyCatcherThumbnail,
+    ],
+  },
+  {
+    title: "RPG Clicker",
+    platform: "PC",
+    engine: "Unity",
+    language: "C#",
+    description: "An RPG clicker game that includes enemies, bosses and loot. Collect loot to be able to do more damage to your enemies.",
+    link: "https://lionstargames.itch.io/rpg-clicker",
+    screenshots: [
+      RPGClickerThumbnail,
+    ],
+  },
+  {
+    title: "Wave-based RPG Survival",
+    platform: "PC",
+    engine: "Unity",
+    language: "C#",
+    description: "This is a first-person zombie survival game created in Unity that was made for a college assignment. In this game, you fight waves of enemies whilst also being able to unlock new areas of the map, get new weapons with an RPG loot system, weapon upgrades and boss fights.",
+    screenshots: [
+      WaveSurvivalThumbnail,
+    ],
+  },
+  {
+    title: "Escape Room",
+    platform: "PC",
+    engine: "Unity",
+    language: "C#",
+    description: "This is an escape room created in Unity that was made for a college assignment. This game contains multiple rooms that the player has to figure out how to get through. Each room contains one or more puzzles in order for them to complete to advance further into the level.",
+    screenshots: [
+      EscapeRoomThumbnail,
+    ],
+  },
+  {
+    title: "2D Platformer",
+    platform: "Android",
+    engine: "Unity",
+    language: "C#",
+    description: "This is a 2D platformer project created in Unity that was made for a college assignment. You can find your way around the level collecting coins and opening crates get loot. You are also able to rotate the entire level to access areas you would not normally be able to.",
+    screenshots: [
+      AndroidPlatformerThumbnail,
+    ],
+  },
+  {
+    title: "Tile-based Prototype Project",
+    platform: "PC",
+    engine: "Monogame",
+    language: "C#",
+    description: "This was a prototype project made within a group of 3 (including myself) for a college assignment. (ADD MORE)",
+    screenshots: [
+      MonogameProjectThumbnail,
+    ],
+  },
+  {
+    title: "VR Shooter Game",
+    platform: "Oculus",
+    engine: "Unity",
+    language: "C#",
+    description: "This is a VR shooter game created in Unity that was made for a college assignment. This game contains 3 levels in which the player has to shoot all targets to complete each level and progress further.",
+    screenshots: [
+      VRShooterThumbnail,
+    ],
+  },
+  {
+    title: "Procedural Terrain Generation Tool",
+    platform: "PC",
+    engine: "Unity",
+    language: "C#",
+    description: "This is a Unity tool that I made for my dissertation project. This allows users to create procedurally generated terrain with foliage and textures within a couple minutes.",
+    screenshots: [
+      ProceduralToolThumbnail,
+    ],
+  },
+  {
+    title: "Customizable Levelling System v2.0",
+    platform: "PC",
+    engine: "Unity",
+    language: "C#",
+    description: "This is a Unity package that I created and uploaded onto the Unity Asset Store. This is a levelling system that can be easily integrated into any game project.",
+    link: "https://assetstore.unity.com/packages/tools/gui/customizable-levelling-system-v2-120348",
+    screenshots: [
+      CLSThumbnail,
+    ],
+  },
+];
